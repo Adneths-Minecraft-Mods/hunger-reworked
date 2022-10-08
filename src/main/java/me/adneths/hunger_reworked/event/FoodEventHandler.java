2275c7dec4bc6c93cc4482c9e420731da09c5ad7
Adneths <bill.lui0921@gmail.com>
2022-10-08 01:23:14 -0700

    Fixed crash on dedicated server

--- src/main/java/me/adneths/hunger_reworked/event/FoodEventHandler.java
+++ src/main/java/me/adneths/hunger_reworked/event/FoodEventHandler.java
@@ -1,16 +1,10 @@
 package me.adneths.hunger_reworked.event;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-
 import me.adneths.hunger_reworked.HungerReworked;
 import me.adneths.hunger_reworked.capability.PlayerStomach;
 import me.adneths.hunger_reworked.capability.PlayerStomach.Food;
 import me.adneths.hunger_reworked.capability.PlayerStomachProvider;
-import me.adneths.hunger_reworked.init.ClientSide;
-import me.adneths.hunger_reworked.init.Registration;
 import net.minecraft.advancements.CriteriaTriggers;
-import net.minecraft.client.gui.GuiComponent;
-import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
@@ -19,7 +13,6 @@ import net.minecraft.stats.Stats;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -28,7 +21,6 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CakeBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
-import net.minecraftforge.client.gui.IIngameOverlay;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.ForgeEventFactory;
@@ -41,32 +33,6 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class FoodEventHandler
 {
-	protected static final ResourceLocation TEXTURE_LOCATION = new ResourceLocation(HungerReworked.MODID, "textures/gui/stomach.png");
-	public static IIngameOverlay STOMACH_OVERLAY = (gui, poseStack, partialTicks, width, height) -> {
-		Player player = ClientSide.minecraft.player;
-		if (player.getAbilities().instabuild || player.isSpectator())
-			return;
-		player.getCapability(PlayerStomachProvider.PLAYER_STOMACH).ifPresent((stomach) -> {
-			int foodAmount = stomach.totalFood;
-			
-			MobEffectInstance ss = player.getEffect(Registration.STRONG_STOMACH.get());
-			int ssa = ss == null ? 0 : ss.getAmplifier() + 1;
-
-			int left = width / 2 + 91;
-			int top = height - gui.right_height;
-			RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-			RenderSystem.setShader(GameRenderer::getPositionTexShader);
-			RenderSystem.setShaderTexture(0, TEXTURE_LOCATION);
-			int maxAmount = Math.max(foodAmount, 20 + ssa * 8);
-			for (int j = 0; j < Math.max(1, maxAmount + 19 / 20); j++)
-				for (int i = 0; i < Math.min(10, j == 0 ? 10 : ((maxAmount + 1) / 2) - j * 10); i++)
-					GuiComponent.blit(poseStack, left - 9 - 8 * i, top - 10 * j, 8, 8, foodAmount - i * 2 - j * 20 < 1 ? 0 : foodAmount - i * 2 - j * 20 == 1 ? 16 : 32,
-							j > 0 && i + j * 10 - 10 < ssa * 4 ? 16 : 0, 16, 16, 48, 32);
-
-			gui.right_height += 10 * (1 + (foodAmount - 1) / 20);
-		});
-		
-	};
 
 	@SubscribeEvent
 	public static void onAttachCapabilitiesPlayer(AttachCapabilitiesEvent<Entity> event)
--- src/main/java/me/adneths/hunger_reworked/init/ClientSide.java
+++ src/main/java/me/adneths/hunger_reworked/init/ClientSide.java
@@ -1,8 +1,17 @@
 package me.adneths.hunger_reworked.init;
 
-import me.adneths.hunger_reworked.event.FoodEventHandler;
+import com.mojang.blaze3d.systems.RenderSystem;
+
+import me.adneths.hunger_reworked.HungerReworked;
+import me.adneths.hunger_reworked.capability.PlayerStomachProvider;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.client.gui.ForgeIngameGui;
+import net.minecraftforge.client.gui.IIngameOverlay;
 import net.minecraftforge.client.gui.OverlayRegistry;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -12,11 +21,37 @@ public class ClientSide
 	public static Minecraft minecraft;
 	public static boolean isClient = false;
 	
+	protected static final ResourceLocation TEXTURE_LOCATION = new ResourceLocation(HungerReworked.MODID, "textures/gui/stomach.png");
+	public static IIngameOverlay STOMACH_OVERLAY = (gui, poseStack, partialTicks, width, height) -> {
+		Player player = ClientSide.minecraft.player;
+		if (player.getAbilities().instabuild || player.isSpectator())
+			return;
+		player.getCapability(PlayerStomachProvider.PLAYER_STOMACH).ifPresent((stomach) -> {
+			int foodAmount = stomach.totalFood;
+			
+			MobEffectInstance ss = player.getEffect(Registration.STRONG_STOMACH.get());
+			int ssa = ss == null ? 0 : ss.getAmplifier() + 1;
+
+			int left = width / 2 + 91;
+			int top = height - gui.right_height;
+			RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+			RenderSystem.setShader(GameRenderer::getPositionTexShader);
+			RenderSystem.setShaderTexture(0, TEXTURE_LOCATION);
+			int maxAmount = Math.max(foodAmount, 20 + ssa * 8);
+			for (int j = 0; j < Math.max(1, maxAmount + 19 / 20); j++)
+				for (int i = 0; i < Math.min(10, j == 0 ? 10 : ((maxAmount + 1) / 2) - j * 10); i++)
+					GuiComponent.blit(poseStack, left - 9 - 8 * i, top - 10 * j, 8, 8, foodAmount - i * 2 - j * 20 < 1 ? 0 : foodAmount - i * 2 - j * 20 == 1 ? 16 : 32,
+							j > 0 && i + j * 10 - 10 < ssa * 4 ? 16 : 0, 16, 16, 48, 32);
+
+			gui.right_height += 10 * (1 + (foodAmount - 1) / 20);
+		});
+	};
+	
 	@SubscribeEvent
 	public static void init(final FMLClientSetupEvent event)
 	{
 		minecraft = Minecraft.getInstance();
 		isClient = true;
-		OverlayRegistry.registerOverlayAbove(ForgeIngameGui.FOOD_LEVEL_ELEMENT, "stomach_hud", FoodEventHandler.STOMACH_OVERLAY);
+		OverlayRegistry.registerOverlayAbove(ForgeIngameGui.FOOD_LEVEL_ELEMENT, "stomach_hud", STOMACH_OVERLAY);
 	}
 }
